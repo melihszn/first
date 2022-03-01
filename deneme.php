99e6ad515a24fed2b704f8528010aea046660284
melihszn <melih55m@.com>
2022-03-01 15:07:08 +0300

    post ve get

--- deneme.php
+++ deneme.php
@@ -1,9 +1,22 @@
 <?php
 echo "melih";
 
+
+echo "<pre>";
+print_r($_POST);
+echo "</pre>";
+
+
 for($i = 0; $i < 10; $i++){
     for($j = 0; $j < $i; $j++){
         echo "*";
     }
     echo "<br>";
 }
+?>
+
+<form action="" method="POST">
+    Ad <input type="text" name="ad" placeholder="Adınızı Girin">
+    Soyad <input type="text" name="soyad" placeholder="SoyAdınızı Girin">
+    <input type="submit" value="Formu Gönder" name="" >
+</form> 
