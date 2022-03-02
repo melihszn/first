9d9be0710e7f7a80c79f18fe12a17fcb532fac0b
melihszn <melih55m@.com>
2022-03-02 13:39:14 +0300

    html

--- kullanicigirisi.php
+++ kullanicigirisi.php
@@ -10,12 +10,37 @@ echo "<h3>User Login<h/3>"; echo "<hr>";
 
 
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style = "background:orange">
+    <form action="kullanicipaneli.php" method="POST">
+    <label for="kullanici_ad" style = "color:white">Username</label>
+     <input type="text" name="kullanici_ad" placeholder="enter your username...">
+     <label for="kullanici_sifre" style = "color:white">Password</label>
+     <input type="password" name="kullanici_sifre" placeholder="enter your password...">
+    <button style ="background:black; color:white">Login</button>
+    </form>
+</body>
+</html>
 
 
-<form action="kullanicipaneli.php" method="POST">
-Username <input type="text" name="kullanici_ad" placeholder="enter your username...">
-Password <input type="password" name="kullanici_sifre" placeholder="enter your password...">
-<button>Login</button>
-</form>
 
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
